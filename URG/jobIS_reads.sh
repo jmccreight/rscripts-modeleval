ea8597608078d49c55bcfad399e8efe4f826c1d9
Aubrey Dugger <adugger@yslogin4.(none)>
2015-09-09 15:53:29 -0600

    Update job scripts

--- URG/jobIS_reads.sh
+++ URG/jobIS_reads.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-bsub -Is -q geyser -W 6:00 -n 16 -P P48500028 -J "rcmd" $SHELL < Rcmd_reads.sh; bkill -J "rcmd"
+bsub -Is -q geyser -W 6:00 -n 16 -P ######## -J "rcmd" $SHELL < Rcmd_reads.sh; bkill -J "rcmd"
 
